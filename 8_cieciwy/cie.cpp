9879abf07dc34db01c93154cd9ee6df63f263e50
Cezary Bartoszuk <cbart@students.mimuw.edu.pl>
2009-12-04 16:18:24 +0100

    Task 8: The Chords working now. (Even documented :)

--- 8_cieciwy/cie.cpp
+++ 8_cieciwy/cie.cpp
@@ -1,56 +1,92 @@
+// ASD-LAB 2009: TASK 8.
+// AUTHOR: Cezary Bartoszuk <cbart@students.mimuw.edu.pl>
+// ID: cb277617
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <deque>
 #include <utility>
 #include <functional>
-#include <cassert>
 
 using namespace std;
 
-typedef long long integer;
+// Integer types.
+typedef long integer;
+typedef long long big_integer;
 
-typedef pair<integer, integer> ii;
-typedef pair<ii, integer> iii;
-typedef vector<iii> viii;
+// Non-integer types.
+typedef pair<integer, integer> ii; //  Pair of integers - a chord.
+typedef pair<ii, integer> iii;  // Triple of integers - a chord with colour.
+typedef vector<iii> viii;  // Sequence of triples of integers.
 
+// Computes maximum of given integers.
+inline integer max(integer i, integer j)
+{
+    return (i > j) ? i : j;
+}
+
+// Computes average of given integers.
+inline integer avg(integer i, integer j)
+{
+    return (i + j) / 2;
+}
+
+// Computes absolute value of given integer.
+inline integer abs(integer i)
+{
+    return max(i, -i);
+}
+
+// Multi-interval tree.
 class itree
 {
+
     private:
+
+        // Sequence of integers.
         typedef vector<integer> vi;
-        vi nodes; // nodes[0] unused;
-        integer n;
+
+        // Tree is stored in an array like in heap implementation.
+        // `nodes[0]` is quantum of max interval, eg. [1..nodes[0]]
+        // is max we can store here.
+        vi nodes;
+
+        // Sequence of modified elements.
+        deque<integer> modified;
+
+        // Computes index of parent of element with given index `i`.
         static inline integer parent(integer i) { return i / 2; }
+
+        // Computes index of left son of element with given index `i`.
         static inline integer left(integer i) { return 2 * i; }
+
+        // Computes index of right son of element with given index `i`.
         static inline integer right(integer i) { return 2 * i + 1; }
-        static inline integer min(integer i, integer j)
-        {
-            return (i < j) ? i : j;
-        }
-        static inline integer max(integer i, integer j)
-        {
-            return (i > j) ? i : j;
-        }
-        static inline integer avg(integer i, integer j)
-        {
-            return (i + j) / 2;
-        }
-        static inline integer abs(integer i) { return max(i, -i); }
+
     public:
-        itree(integer n)
-            : nodes(2 * n + 1, 0),
-              n(n)
+
+        // Creates itree with maximal interval stored [1..quantity].
+        //   Time: O(quantity).
+        itree(integer quantity)
+            : nodes(2 * quantity + 1, 0),
+              modified()
         {
+            nodes[0] = quantity;
         }
+
+        // Inserts `val` of prefixex with given end to `self`.
+        //   Time: O(log n) where `n` is size of itree.
         void push_prefix(integer prefix_end_inclusive, integer val)
         {
-            integer ibegin = 1, iend = n;
+            integer ibegin = 1, iend = nodes[0];
             integer index = 1;
-            while(true) {
-                //tu jest źle....
+            while(index <= 2 * nodes[0]) {
                 integer med = avg(ibegin, iend);
                 if(prefix_end_inclusive <= med) {
                     if(ibegin == iend) {
                         nodes[index] += val;
+                        modified.push_back(index);
                         break;
                     }
                     else {
@@ -61,23 +97,27 @@ class itree
                 else { // prefix_end_inclusive > med
                     if(prefix_end_inclusive >= iend) { // ==
                         nodes[index] += val;
+                        modified.push_back(index);
                         break;
                     }
                     else {
                         nodes[left(index)] += val;
+                        modified.push_back(left(index));
                         ibegin = med + 1;
                         index = right(index);
                     }
                 }
             }
         }
+
+        // Computes how many sets which contains `i` are in `self`.
+        //   Time: pesimistic O(lg n) where `n` is size of itree.
         integer count(integer i) const
         {
-            integer ibegin = 1, iend = n;
+            integer ibegin = 1, iend = nodes[0];
             integer index = 1;
             integer counter = 0;
-            while(index <= 2 * n) {
-                //assert(index <= 2 * n);
+            while(index <= 2 * nodes[0]) {
                 counter += nodes[index];
                 integer med = avg(ibegin, iend);
                 if(i <= med) {
@@ -91,34 +131,44 @@ class itree
             }
             return counter;
         }
+
+        // Clears the set.
+        //   Time: pesimistic O(m log n) where `m` is # of inserts
+        //       and `n` is size of itree.
         void clear() {
-            nodes.assign(2 * n + 1, 0);
+            while(!modified.empty()) {
+                nodes[modified.front()] = 0;
+                modified.pop_front();
+            }
+
         }
-        friend ostream& operator<<(ostream& os, const itree& i);
+
 };
 
-ostream& operator<<(ostream& os, const itree& i)
-{
-    for(integer j = 1; j <= i.n; j ++)
-        os << i.count(j) << '\n';
-    return os;
-}
-
+// Compares colours of given chords.
 bool colour_cmp(iii one, iii two)
 {
     return one.second < two.second;
 }
 
+// Indicates if `element` is in range described by `from` and `to`.
+// The range is left-inclusive and right-exclusive (as always).
+inline bool in_range(integer element, integer from, integer to)
+{
+    return (from <= element) && (element < to);
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);  // For speed reasons ;).
 
-    integer n, k;
+    // Get all data from stdin.
+    integer n, k;  // Chords and colours quantity.
     cin >> n;
     cin >> k;
 
     viii circle;
-    circle.reserve(n);
+    circle.reserve(n);  // Here we store the chords.
 
     for(integer i = 0; i < n; i ++) {
         integer a, b, c;
@@ -131,32 +181,46 @@ int main()
             circle.push_back(make_pair(make_pair(b, a), c));
     }
 
-    integer counter = 0;
+    // Here we'll be counting the chord crosses.
+    big_integer counter = 0;
 
-    sort(circle.begin(), circle.end(), less<iii>()); // sorting with first coord
+    // Sorting with first coordinate, ascending.
+    sort(circle.begin(), circle.end(), less<iii>());
 
-    itree all(2 * n);
+    // Here we'll compute crossing all chords.
+    itree chords(2 * n);
     for(viii::const_iterator iter = circle.begin();
-            iter != circle.end(); iter ++) {
-        counter += all.count(iter->first.first) - all.count(iter->first.second);
-        //all.push_prefix(iter->first.second, 1);
+            iter != circle.end();
+            iter ++) {
+        // Add crosses.
+        counter += chords.count(iter->first.first)
+            - chords.count(iter->first.second);
+        chords.push_prefix(iter->first.second, 1);
     }
 
-    stable_sort(circle.begin(), circle.end(), colour_cmp); // sorting with col
+    // Sorting with colours, stable, ascending.
+    stable_sort(circle.begin(), circle.end(), colour_cmp);
 
-    all.clear();
+    // Clear all prefixes from `chords`.
+    chords.clear();
+    // Set the first colour.
     integer cur_colour = circle.begin()->second;
+    // Here we'll compute crossing chords with same colour.
     for(viii::const_iterator iter = circle.begin();
             iter != circle.end(); iter ++) {
-        if(iter->second != cur_colour) {
-            all.clear();
+        if(cur_colour != iter->second) {  // When colour changes:
+            chords.clear();
             cur_colour = iter->second;
         }
-        counter -= all.count(iter->first.first) - all.count(iter->first.second);
-        all.push_prefix(iter->first.second, 1);
+        // Subtract crosses.
+        counter -= chords.count(iter->first.first)
+            - chords.count(iter->first.second);
+        chords.push_prefix(iter->first.second, 1);
     }
 
+    // Print the result.
     cout << counter << flush;
 
     return 0;
-}
+
+}  ///:~
